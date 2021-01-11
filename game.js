497ee4e8142844b2c71e051d404bb571e5039fb6
ModernShrub <75245177+ModernShrub@users.noreply.github.com>
2021-01-11 15:06:15 +0530

    Update game.js

--- game.js
+++ game.js
@@ -26,8 +26,8 @@ function send() {
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row =  question_number + input_box + check_button ; 
     document.getElementById("output").innerHTML = row;
-	document.getElementById("number1").value = "";
-	document.getElementById("number2").value = "";
+	document.getElementById("num1").value = "";
+	document.getElementById("num2").value = "";
 }
 
 
